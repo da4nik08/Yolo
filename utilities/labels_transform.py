291e72f2043bc54b980c86d024ac92fb884277cf
da4nik08 <illia.cherniavskyi1@nure.ua>
2023-12-10 23:09:53 +0200

    prepare label

--- utilities/labels_transform.py
+++ utilities/labels_transform.py
@@ -6,7 +6,7 @@ def transform_label(label, class_ind):
     for l in label:
         temp_list = [1.0]
         C, x, y, w, h = map(float, l.split(" "))
-        print(f"Class-{C}, x-{x}, y-{y}, w-{w}, h-{h}")
+        #print(f"Class-{C}, x-{x}, y-{y}, w-{w}, h-{h}")
         temp_list = temp_list + [x, y, w, h] + ([0.0] * len(class_ind))
         temp_list[4 + class_ind.index(C) + 1] = 1.0
         output_list.append(temp_list)
@@ -18,6 +18,7 @@ def Yolo_format_label(labels, n, anchor):
 
     # Assign the labels to the array based on the structure provided
     anchor_index = np.zeros((n, n))
+    count = 0
     
     for i in range(len(labels)):
         label = labels[i]
@@ -26,16 +27,20 @@ def Yolo_format_label(labels, n, anchor):
         # Calculate grid cell indices for x and y values
         cell_x = int(x * n)
         cell_y = int(y * n)
+
+        if cell_x == n: cell_x = n-1
+        if cell_y == n: cell_y = n-1
         
         # Fill in the values in the yolo_labels array
         if anchor_index[cell_x, cell_y] + len(label) > yolo_labels.shape[2]:
-            print(F"You need more anchor on cell {cell_x}, {cell_y}")
-            print(f"anchor_index[cell_x, cell_y] = {anchor_index[cell_x, cell_y]}")
+            #print(F"You need more anchor on cell {cell_x}, {cell_y}")
+            #print(f"anchor_index[cell_x, cell_y] = {anchor_index[cell_x, cell_y]}")
+            count += 1
         else:
             yolo_labels[cell_x, cell_y, int(anchor_index[cell_x, cell_y]):
                                         int(anchor_index[cell_x, cell_y]) + len(label)] = label
 
         # Calculate the anchor index based on object detectability in one cell
         anchor_index[cell_x, cell_y] += len(label)
-
-    return yolo_labels
+    
+    return yolo_labels, count
